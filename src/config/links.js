945727053179b6d64ee40b2f81ceb0754142bf97
HerlyRyan <herlyryanth@gmail.com>
2026-09-07 09:05:33 +0800

    modified: data links

--- src/config/links.js
+++ src/config/links.js
@@ -34,48 +34,256 @@ export const mainLinks = [
 
   {
     id: "google-drive-2",
-    title: "Angket Blok 2026",
+    title: "Angket Blok IKM 2026",
     description: "Akses dokumen DPM FK UNTAR melalui Google Drive.",
     icon: FiFolder,
     type: "external",
-    url: "https://drive.google.com/drive/folders/1FHrCPWScpfAcsZp8yC68OILTWAYK8mOa",
+    url: "https://docs.google.com/forms/d/e/1FAIpQLSculv14CgG5ylSDCiUSD6wAICagK6MvJTssssz6i2fZUpKaYQ/viewform?usp=sharing&ouid=113369188841890918336",
   },
 
   {
     id: "google-drive-3",
-    title: "Angket Blok 2025",
+    title: "Angket Blok BM3 2027",
     description: "Akses dokumen DPM FK UNTAR melalui Google Drive.",
     icon: FiFolder,
     type: "external",
-    url: "https://drive.google.com/drive/folders/1nZclPvQ3Z6r2QwlJQYarY_fGU9ZYZo2i",
+    url: "https://docs.google.com/forms/d/e/1FAIpQLScxjVR8zC5h1WJsB3pgVDLJAHaPi5MBoy2rpI1mWn8g8Kit6g/viewform?usp=sharing&ouid=113369188841890918336",
   },
 
   {
     id: "google-drive-4",
-    title: "Angket Blok 2024",
+    title: "Angket Blok BM2 2026",
     description: "Akses dokumen DPM FK UNTAR melalui Google Drive.",
     icon: FiFolder,
     type: "external",
-    url: "https://drive.google.com/drive/folders/1_a4f1Llu_2wodE4YJhaYZQYcf28uCG0G",
+    url: "https://docs.google.com/forms/d/e/1FAIpQLSeoHA8-sh3fbWwO6YmvA-w711kJtiyrVU2sz-MAo4wyKCY88Q/viewform?usp=sharing&ouid=113369188841890918336",
   },
 
   {
     id: "google-drive-5",
-    title: "Angket Blok 2023",
+    title: "Angket Blok BM1 2026",
     description: "Akses dokumen DPM FK UNTAR melalui Google Drive.",
     icon: FiFolder,
     type: "external",
-    url: "https://drive.google.com/drive/folders/1IRqzK1pfSFDESQc4R6R9JBPOkZSa6Yrt",
+    url: "https://docs.google.com/forms/d/e/1FAIpQLSfu9jBVTv30yZ613F-hjFblc4bZu_cJPVTluJDh3TbkhrOMIA/viewform?usp=sharing&ouid=113369188841890918336",
   },
 
   {
     id: "google-drive-6",
-    title: "KOAS",
+    title: "Angket Blok Humaniora 2026",
+    description: "Akses dokumen DPM FK UNTAR melalui Google Drive.",
+    icon: FiFolder,
+    type: "external",
+    url: "https://docs.google.com/forms/d/e/1FAIpQLSehSqEFJIhYpD-JJEH4zcpSwlyYdHvqn2r4MyHzWb0lyJzepw/viewform?usp=sharing&ouid=113369188841890918336",
+  },
+
+  {
+    id: "google-drive-7",
+    title: "Angket Blok BSH 2026",
+    description: "Akses dokumen DPM FK UNTAR melalui Google Drive.",
+    icon: FiFolder,
+    type: "external",
+    url: "https://docs.google.com/forms/d/e/1FAIpQLScnFkS0fYqiBhd-5sub1PHiJpwk2mLcO2LpEiTq9X06eCUfvg/viewform?usp=sharing&ouid=113369188841890918336",
+  },
+
+  // {
+  //   id: "google-drive-",
+  //   title: "Angket Blok 2026",
+  //   description: "Akses dokumen DPM FK UNTAR melalui Google Drive.",
+  //   icon: FiFolder,
+  //   type: "external",
+  //   url: "https://drive.google.com/drive/folders/1FHrCPWScpfAcsZp8yC68OILTWAYK8mOa",
+  // },
+
+  {
+    id: "google-drive-8",
+    title: "Angket Blok Sistim Muskuloskeletal 2027",
+    description: "Akses dokumen DPM FK UNTAR melalui Google Drive.",
+    icon: FiFolder,
+    type: "external",
+    url: "https://docs.google.com/forms/d/e/1FAIpQLSfYx7EkrWq4KrXhuDqZPACWFO921DlOJFTlzPCoQZiMu-R4rQ/viewform?usp=sharing&ouid=113369188841890918336",
+  },
+
+  {
+    id: "google-drive-9",
+    title: "Angket Blok Respirasi 2027",
+    description: "Akses dokumen DPM FK UNTAR melalui Google Drive.",
+    icon: FiFolder,
+    type: "external",
+    url: "https://docs.google.com/forms/d/e/1FAIpQLSf58xmP9vJBbcnkKlr9WdPzRSMwqUtBKFwDs4J4ASy8jlxlSA/viewform?usp=sharing&ouid=113369188841890918336",
+  },
+
+  {
+    id: "google-drive-10",
+    title: "Angket Blok Sistim Kardiovaskular 2027",
+    description: "Akses dokumen DPM FK UNTAR melalui Google Drive.",
+    icon: FiFolder,
+    type: "external",
+    url: "https://docs.google.com/forms/d/e/1FAIpQLSer4TRcpx8J8FnpnCVxBHCciscUHwaRfXAyE0rkh3OmlZu4zw/viewform?usp=sharing&ouid=113369188841890918336",
+  },
+
+  {
+    id: "google-drive-11",
+    title: "Angket Blok Sistim Sistim Imun dan Infeksi 2026",
+    description: "Akses dokumen DPM FK UNTAR melalui Google Drive.",
+    icon: FiFolder,
+    type: "external",
+    url: "https://docs.google.com/forms/d/e/1FAIpQLSevv0-nS4RP63sSij-xr-zfOrmCRDRhQYm_RJY0zYuw2ORk4w/viewform?usp=sharing&ouid=113369188841890918336",
+  },
+
+  {
+    id: "google-drive-12",
+    title: "Angket Blok Sistim Hematologi 2026",
+    description: "Akses dokumen DPM FK UNTAR melalui Google Drive.",
+    icon: FiFolder,
+    type: "external",
+    url: "https://docs.google.com/forms/d/e/1FAIpQLScE-pNHyT8QKRVcZql3Vw0PYMHaMtR9Lw7e2OB3j1eVTDTqtg/viewform?usp=sharing&ouid=113369188841890918336",
+  },
+
+  {
+    id: "google-drive-13",
+    title: "Angket Blok Siklus 2026",
+    description: "Akses dokumen DPM FK UNTAR melalui Google Drive.",
+    icon: FiFolder,
+    type: "external",
+    url: "https://docs.google.com/forms/d/e/1FAIpQLScVOmiyrpA6fpSXrJxWgafpiKiXsz0t33u1z_-bqWUtvOeYlQ/viewform?usp=sharing&ouid=113369188841890918336",
+  },
+
+  // {
+  //   id: "google-drive-3",
+  //   title: "Angket Blok 2025",
+  //   description: "Akses dokumen DPM FK UNTAR melalui Google Drive.",
+  //   icon: FiFolder,
+  //   type: "external",
+  //   url: "https://drive.google.com/drive/folders/1nZclPvQ3Z6r2QwlJQYarY_fGU9ZYZo2i",
+  // },
+
+  {
+    id: "google-drive-14",
+    title: "Angket Blok Proposal Skripsi 2026",
+    description: "Akses dokumen DPM FK UNTAR melalui Google Drive.",
+    icon: FiFolder,
+    type: "external",
+    url: "https://docs.google.com/forms/d/e/1FAIpQLScipWpiHlQfjJNP2KDYq32ldcxZmhYUnyX1NVhs_EkYSP8lOA/viewform?usp=sharing&ouid=113369188841890918336",
+  },
+
+  {
+    id: "google-drive-15",
+    title: "Angket Blok Sistem Gastrointestinal 2026",
+    description: "Akses dokumen DPM FK UNTAR melalui Google Drive.",
+    icon: FiFolder,
+    type: "external",
+    url: "https://docs.google.com/forms/d/e/1FAIpQLSe5cHO1PE9xAb6bGDfoWGKV3ayg_FpKp7oxYBwtSYzgifzkWQ/viewform?usp=sharing&ouid=113369188841890918336",
+  },
+
+  {
+    id: "google-drive-16",
+    title: "Angket Blok Sistem Reproduksi 2027",
+    description: "Akses dokumen DPM FK UNTAR melalui Google Drive.",
+    icon: FiFolder,
+    type: "external",
+    url: "https://docs.google.com/forms/d/e/1FAIpQLSc96UzzltSen2mnR526WYn4QdogcgF6_FPM_1TB8AgC4_ZCXw/viewform?usp=sharing&ouid=113369188841890918336",
+  },
+
+  {
+    id: "google-drive-17",
+    title: "Angket Blok Sistem Saraf dan Kejiwaan 2027",
+    description: "Akses dokumen DPM FK UNTAR melalui Google Drive.",
+    icon: FiFolder,
+    type: "external",
+    url: "https://docs.google.com/forms/d/e/1FAIpQLSdkI1Sucvh9GcnANisRnftLmJ9oL_favt_23v8xGWnTBtR3lA/viewform?usp=sharing&ouid=113369188841890918336",
+  },
+
+  {
+    id: "google-drive-18",
+    title: "Angket Blok Sistem Urogenital 2027",
+    description: "Akses dokumen DPM FK UNTAR melalui Google Drive.",
+    icon: FiFolder,
+    type: "external",
+    url: "https://docs.google.com/forms/d/e/1FAIpQLSd0Te9bkXlNmsS8U2U3aqKWfIX30OkxodTiPXiEuwHaKhqYOg/viewform?usp=sharing&ouid=113369188841890918336",
+  },
+
+  {
+    id: "google-drive-19",
+    title: "Angket Blok Sistem Endokrin dan Metabolisme 2026",
+    description: "Akses dokumen DPM FK UNTAR melalui Google Drive.",
+    icon: FiFolder,
+    type: "external",
+    url: "https://docs.google.com/forms/d/e/1FAIpQLScqKsPaD-T5kc4BGahzUmI4WjXVMi5k28Fx49fXjpFegCzWtA/viewform?usp=sharing&ouid=113369188841890918336",
+  },
+
+  {
+    id: "google-drive-20",
+    title: "Angket Blok Sistem Hepatobilier 2026",
+    description: "Akses dokumen DPM FK UNTAR melalui Google Drive.",
+    icon: FiFolder,
+    type: "external",
+    url: "https://docs.google.com/forms/d/e/1FAIpQLSfdDjvr84m_9EESZjVvSvGbIWlZOMxaicoTxhq7tFGR8cgaAA/viewform?usp=sharing&ouid=113369188841890918336",
+  },
+
+  // {
+  //   id: "google-drive-4",
+  //   title: "Angket Blok 2024",
+  //   description: "Akses dokumen DPM FK UNTAR melalui Google Drive.",
+  //   icon: FiFolder,
+  //   type: "external",
+  //   url: "https://drive.google.com/drive/folders/1_a4f1Llu_2wodE4YJhaYZQYcf28uCG0G",
+  // },
+
+  {
+    id: "google-drive-21",
+    title:
+      "Angket Blok Etika Kedokteran, Hukum Kedokteran dan Kedokteran Forensik 2026",
+    description: "Akses dokumen DPM FK UNTAR melalui Google Drive.",
+    icon: FiFolder,
+    type: "external",
+    url: "https://docs.google.com/forms/d/e/1FAIpQLScgbdHqbFudHzKBuzFfEs-Yg-x6X6xF1eVg6XJq9VnRA-A6mA/viewform?usp=sharing&ouid=113369188841890918336",
+  },
+
+  {
+    id: "google-drive-22",
+    title: "Angket Blok Kegawatdaruratan Medik 2026",
+    description: "Akses dokumen DPM FK UNTAR melalui Google Drive.",
+    icon: FiFolder,
+    type: "external",
+    url: "https://docs.google.com/forms/d/e/1FAIpQLSdFSZI2JiCUkOjmrVAMULYhnJsGbNOdoikDD6SljO32cxteew/viewform?usp=sharing&ouid=113369188841890918336",
+  },
+
+  {
+    id: "google-drive-23",
+    title: "Angket Blok Penginderaan 2026",
+    description: "Akses dokumen DPM FK UNTAR melalui Google Drive.",
+    icon: FiFolder,
+    type: "external",
+    url: "https://docs.google.com/forms/d/e/1FAIpQLScqhcTbgaCEKhRwegwRywmHyl3cjU6K5Zz7MvXyTGcakVSNJg/viewform?usp=sharing&ouid=113369188841890918336",
+  },
+
+  // {
+  //   id: "google-drive-5",
+  //   title: "Angket Blok 2023",
+  //   description: "Akses dokumen DPM FK UNTAR melalui Google Drive.",
+  //   icon: FiFolder,
+  //   type: "external",
+  //   url: "https://drive.google.com/drive/folders/1IRqzK1pfSFDESQc4R6R9JBPOkZSa6Yrt",
+  // },
+
+  {
+    id: "google-drive-24",
+    title: "Angket Evaluasi Kepaniteraan 2026",
     description: "Akses dokumen DPM FK UNTAR melalui Google Drive.",
     icon: FiFolder,
     type: "external",
     url: "https://drive.google.com/drive/folders/1QSM77IoFphLDx3aKv5TYN0VQwj2vI2pY",
   },
+
+  // {
+  //   id: "google-drive-6",
+  //   title: "KOAS",
+  //   description: "Akses dokumen DPM FK UNTAR melalui Google Drive.",
+  //   icon: FiFolder,
+  //   type: "external",
+  //   url: "https://drive.google.com/drive/folders/1QSM77IoFphLDx3aKv5TYN0VQwj2vI2pY",
+  // },
 ];
 
 export const socialLinks = [
