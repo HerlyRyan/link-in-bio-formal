bd1c9aa37724623341a396e814ddaf6fd7833a71
HerlyRyan <herlyryanth@gmail.com>
2026-09-09 09:18:22 +0800

    modified: change the value of vision and mission key data

--- src/config/visionMission.js
+++ src/config/visionMission.js
@@ -2,12 +2,12 @@ export const visionMissionConfig = {
   title: "Visi, Misi, & Fungsi DPM",
 
   vision:
-    "Menjadikan DPM FK UNTAR sebagai lembaga legislatif yang aspiratif, transparan, progresif, dan berlandaskan kekeluargaan.",
+    "Menjadikan DPM FK UNTAR sebagai lembaga legislatif yang profesional dan berintegritas dalam menjalankan fungsi dan tugasnya serta menjalin solidaritas dan kolaborasi untuk tujuan bersama",
 
   missions: [
-    "Menampung dan menyalurkan aspirasi mahasiswa FK UNTAR secara aktif dan solutif.",
-    "Melakukan fungsi pengawasan terhadap kinerja lembaga kemahasiswaan secara objektif.",
-    "Mempererat sinergi dan keharmonisan antar seluruh civitas akademika FK UNTAR.",
+    "Menjadi jembatan advokasi yang tanggap, kritis, dan solutif, dalam mengawal isu-isu akademik, fasilitas, maupun kesejahteraan mahasiswa FK UNTAR",
+    "Mendorong kolaborasi yang harmonis dan komunikatif antara DPM, BEM, UKMF, dan pihak fakultas FK UNTAR",
+    "Membangung internal DPM FK yang profesional, berintegritas, dan suportif melalui pembagian kerja yang terstruktur dan evaluasi yang berkesinambungan",
   ],
 
   functions: [
